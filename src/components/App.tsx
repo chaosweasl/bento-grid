de7dab6438b54355e400b58001ccdce36006e7a6
Serban Iacob <iacobserban17danieloff@gmail.com>
2025-03-02 06:53:35 +0200

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="flex items-center h-screen justify-center">
+        <h1 className="text-black italic text-5xl">Hello World</h1>
+      </div>
+    </>
+  );
+}
