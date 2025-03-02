e88dd4abe0b484c9f101be03a9ffb7c1e5cab942
Serban Iacob <iacobserban17danieloff@gmail.com>
2025-03-02 14:53:48 +0200

    added 3 items, custom colors + fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,56 @@
-//import.meta.glob is lazy-loading but eager: true sets it to import at build time
-const images = import.meta.glob("../assets/images/*.webp", { eager: true });
+import item1img from "../assets/images/illustration-create-post.webp";
+import item2img from "../assets/images/illustration-five-stars.webp";
+import item3img from "../assets/images/illustration-schedule-posts.webp";
+import item4img from "../assets/images/illustration-ai-content.webp";
+import item5img from "../assets/images/illustration-multiple-platforms.webp";
+import item6img from "../assets/images/illustration-consistent-schedule.webp";
+import item7img from "../assets/images/illustration-audience-growth.webp";
+import item8img from "../assets/images/illustration-grow-followers.webp";
 
 import "../index.css";
 
 export default function App() {
   return (
     <>
-      <div className="flex items-center h-screen justify-center">
-        <h1 className="text-black italic text-5xl">Hello World</h1>
+      <div className="grid-container grid place-items-center h-screen">
+        {/* <div
+          className="item1 bg-[#faeee2] w-60 h-80 flex flex-col items-top 
+        items-center justify-center rounded-2xl"
+        >
+          <h1 className="text-4xl font-medium font-[DMSans] text-left justify-center mt-2 m-5">
+            Create and schedule content{" "}
+            <span className="italic cpurple">quicker.</span>
+          </h1>
+          <img className="img1 w-40" src={item1img} />
+        </div>
+
+        <div
+          className="item2 bg-[#7651dc] w-80 h-50 flex flex-col items-center 
+        justify-center text-center rounded-2xl"
+        >
+          <h1 className="text-4xl font-medium font-[DMSans] text-white">
+            Social Media <span className="cyellow">10x</span>{" "}
+            <span className="italic">Faster</span> with AI
+          </h1>
+          <img className="img2 w-40 mt-4" src={item2img} />
+          <h2 className="text-sm font-[DMSans] text-gray-300">
+            Over 4,000 5-star reviews
+          </h2>
+        </div> */}
+
+        {/* <div
+          className="item3 bg-[#dcd1fc] w-100 h-125 flex-col items-center 
+        justify-center text-center rounded-2xl"
+        >
+          <h1 className="text-2xl font-medium font-[DMSans] text-black m-5">
+            Schedule to social media.
+          </h1>
+          <img className="img3 w-90 m-5" src={item3img} />
+          <h2 className="text-lg font-[DMSans] text-black m-5">
+            Optimise post timings to publish content at the perfect time for
+            your audience.
+          </h2>
+        </div> */}
       </div>
     </>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        dm: ["DMSans", "sans-serif"],
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        bold: 700,
+      },
+    },
+  },
+  plugins: [],
+};
